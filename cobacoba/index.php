3fca30e451b71f34afa6b1eca09da48f500ce4f7
Intan Ayu <intanayumulya7@gmail.com>
2020-02-11 15:33:25 +0700

    update bootsrap

--- cobacoba/index.php
+++ cobacoba/index.php
@@ -8,6 +8,7 @@
 
 
 <br>
+<br>
 <?php
 // penggunaan operator modulus/sisa hasil bagi
 $hal=11;
@@ -41,5 +42,158 @@ if($hasil==80){
     echo "nilai anda adalah $kurang";
 }elseif($hasil==2){
     echo "nilai anda adalah $bagi";
+}else{
+    echo "nilai yang anda masukkan tidak tersedia";
 }
+
+?>
+
+
+<!-- menentukan nilai biodata  -->
+<br>
+<br>
+<?php
+$nim="1602110134";
+$nama='Intan ayu mulya pratiwi';
+$umur="21 Tahun";
+$nilai=90;
+$status=TRUE;
+
+echo "nim :".$nim."<br>";
+echo "nama:$nama <br>";
+print "umur: $umur <br>";
+print "umur: ".$umur; print "<br>";
+printf ("nilai : %.1f<br>", $nilai);
+if($status)
+echo "status: aktif";
+else
+echo "status : tidak aktif";
+?>
+
+<!-- membuat define variable dan nilai -->
+<br>
+<br>
+<?php
+define ("nama", "Intan ayu mulya pratiwi");
+define ("nilai",90);
+
+//nama="intan";// akan menyebabkan eror
+echo "nama:".nama;
+echo "<br> nilai:". nilai;
+
+?>
+
+<br>
+<br>
+<?php
+$gaji=1000000;
+$pajak=0.025;
+$thp=$gaji-($gaji*$pajak);
+echo "Gaji sebelum pajak=Rp. $gaji <br>";
+echo "gaji yang dibawa pulang=Rp. $thp";
+?>
+
+
+<!-- operator logika dan perbandingan dalam php -->
+<br>
+<br>
+<br>
+<?php
+    $a=5;
+    $b=4;
+
+    echo "$a==$b:". ($a==$b);
+    echo "<br>$a !=$b:". ($a!=$b);
+    echo "<br>$a > $b:". ($a>$b);
+    echo "<br<$a < $b:". ($a<$b);
+    echo "<br> ($a!=$b) and ($a>$b):" . (($a!=$b) and ($a>$b));
+    echo "<br> ($a!=$b) || ($a>$b):" . (($a!=$b) and ($a>$b));
+
+?>
+<!-- percabangan -->
+<br>
+<br>
+<?php
+    $hari= "minggu";
+    if($hari=="senin"){
+        echo "upacara bendera";
+    }elseif($hari=="selasa"){
+        echo "praktikum";
+    }elseif ($hari=="rabu"){
+        echo "latihan";
+    }elseif ($hari=="kamis"){
+        echo "makan";
+    }elseif ($hari=="jumat"){
+        echo "belajar";
+    }elseif ($hari=="sabtu"){
+        echo "bertamasya";
+    }elseif ($hari=="minggu"){
+        echo "berlibur keluar kota";
+    }else{
+        echo "tidak ada acara";
+    }
+?>
+
+<br>
+<br>
+<?php
+    $nim="1602110134";
+    $nama='Intan ayu mulya pratiwi';
+    $jurusan="Akuntansi";
+    $nilai=90;
+
+    if($nilai<=50){
+        echo "Grade yang diperoleh oleh $nama dengan $nim dan $jurusan adalah E";
+    }elseif ($nilai<=65){
+        echo "Grade yang diperoleh oleh $nama dengan $nim dan $jurusan adalah D";
+    }elseif ($nilai<=72){
+        echo "Grade yang diperoleh oleh $nama dengan $nim dan $jurusan adalah C";
+    }elseif ($nilai<=83){
+        echo "Grade yang diperoleh oleh $nama dengan $nim dan $jurusan adalah B";
+    }elseif ($nilai<=100 || $nilai=100){
+        echo "Grade yang diperoleh oleh $nama dengan $nim dan $jurusan adalah A";
+    }else {
+        echo "$nama dengan $nim dan $jurusan dinyatakan tidak lulus ";
+    }
+?>
+
+<br>
+<br>
+<?php
+    $nama="intan ayu mulya pratiwi";
+    $alamat="asrama tni-ad kuta alam";
+    $nohp="085281685155";
+
+    $hasil="";
+    if($hasil=="intan ayu mulya pratiwi"){
+        echo($nama);
+    }elseif($hasil=="asrama tni-ad kuta alam"){
+        echo($alamat);
+    }else{
+        echo($nohp);
+    }
+?>
+
+<br>
+<?php
+$a=10;
+$b=20;
+function mytest() {
+    global $a;
+    global $b;
+    echo ($a)+($b);
+}
+mytest();
+?>
+
+<br>
+<?php
+    global $a;
+    $a="abc";
+    global $b;
+    $b="intan ayu mulya";
+
+    //adalah sama dengan menggunakan perintah:
+    echo $GLOBALS["a"]="abc <br>";
+    echo $GLOBALS["b"]="intan ayu mulya";
 ?>
--- index.php
+++ index.php
@@ -5,44 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css.map">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css.map">
+    <!-- css for reboot -->
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css.map">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css.map">
+    <!-- css for bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css.map">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
 </head>
 <body>
-<?php
-    $nama="intan ayu mulya pratiwi";
-    $alamat="asrama tni-ad kuta alam";
-    $nohp="085281685155";
+    
 
-    $hasil="";
-    if($hasil=="intan ayu mulya pratiwi"){
-        echo($nama);
-    }elseif($hasil=="asrama tni-ad kuta alam"){
-        echo($alamat);
-    }else{
-        echo($nohp);
-    }
-?>
-<br>
-<?php
-$a=10;
-$b=20;
-function mytest() {
-    global $a;
-    global $b;
-    echo ($a)+($b);
-}
-mytest();
-?>
 
-<br>
-<?php
-    global $a;
-    $a="abc";
-    global $b;
-    $b="intan ayu mulya";
 
-    //adalah sama dengan menggunakan perintah:
-    echo $GLOBALS["a"]="abc <br>";
-    echo $GLOBALS["b"]="intan ayu mulya";
-?>
+
+
+<!-- for bundle -->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/bootstrap.bundle.js.map"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js.map"></script>
+    <!-- for bootstrap -->
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.js.map"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js.map"></script>
 </body>
 </html>
