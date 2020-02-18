37bb213953297190673d066f6735910d6ebf187b
Intan Ayu <intanayumulya7@gmail.com>
2020-02-18 15:33:38 +0700

    neww

---
+++ nilaiarray.php
@@ -0,0 +1,38 @@
+<?php
+include 'header.php';
+?>
+
+
+
+
+<?php
+$arrnilai=array("ani"=>80, "otim"=>90, "ana"=>75, "budi"=>85);
+echo "menampilkan isi array dengan foreach:<br>";
+foreach($arrnilai as $nama=>$nilai){
+    echo "nilai $nama=$nilai <br>";
+}
+reset ($arrnilai);
+echo "<br> menampilkan isi array dengan while dan list: <br>";
+while (list ($nama,$nilai)=each ($arrnilai)){
+    echo "nilai $nama=$nilai<br>";
+}
+?>
+
+<?php
+$arrwarna=array ("blue", "black", "red", "yellow", "green");
+$arrnilai=array ("ani"=>80, "otim"=>90, "ana"=>80,"budi"=>85);
+echo "<pre>";
+print_r($arrwarna);
+echo "<br>";
+print_r($arrnilai);
+echo "</pre>";
+?>
+
+
+<br>
+<br>
+<br>
+<br>
+<?php
+include 'footer.php';
+?>
---
+++ teswarna.php
@@ -0,0 +1,34 @@
+<?php
+include 'header.php';
+?>
+
+<form action="" method="POST">
+    <input type="text" name="color">
+    <input type="submit" name="tambah" value="tampil">
+</form>
+
+<?php
+if(isset($_POST['tambah'])){
+    $color=$_POST['color'];
+
+    $arraycolor=[
+        "merah",
+        "hijau",
+        "kuning"
+    ];
+
+    if($color==$arraycolor[0]){
+        echo "<h1 style='color: red'>$color</h1>";
+    }elseif($color==$arraycolor[1]){
+        echo "<h1 style='color: green'>$color</h1>";
+    }elseif($color==$arraycolor[2]){
+        echo "<h1 style='color: yellow'>$color</h1>";
+    }else {
+        echo "<h1 style='color: blue'>$color</h1>";
+    }
+}
+?>
+
+<?php
+include 'footer.php';
+?>
